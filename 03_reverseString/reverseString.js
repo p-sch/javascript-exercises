83d995532b01292207bdcee6e7d5e0cad3c99fc3
Paul <paulschwandt3@gmail.com>
2023-01-23 15:34:46 -0700

    complete reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let strArray = string.split("");
+    let reversedList = [];
+    for (i = 0; i < string.length; i++){
+        reversedList.push(strArray.pop())
+    };
+    return reversedList.join("")
 };
 
 // Do not edit below this line
