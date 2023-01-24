077512b1c044345482addb2f786124b6ba6d64ac
Paul <paulschwandt3@gmail.com>
2023-01-23 22:30:06 -0700

    improve leapyear

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,16 +1,5 @@
 const leapYears = function(year) {
-    if (year % 400 == 0){
-        return true;
-    }
-    if (year % 100 == 0){
-        return false
-    }
-    if (year % 4 == 0){
-        return true
-    }
-    else {
-        return false
-    }
+    return year % 4 === 0 && (year % 100 !== 0 || year % 400 === 0);
 };
 
 // Do not edit below this line
