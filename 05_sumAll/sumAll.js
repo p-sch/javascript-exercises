4cdf914b30fd8d966e56f3ca822ea9b0df0d57b6
Paul <paulschwandt3@gmail.com>
2023-01-23 22:02:51 -0700

    comple SumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function(int1, int2) {
+    if (int1 < 0 || int2 < 0){
+        return 'ERROR'
+    }
+    else if (typeof int1 != 'number' || typeof int2 != 'number'){
+        return 'ERROR'
+    }
+    else {
+        let sum = 0;
+        let start = Math.min(int1, int2);
+        let end = Math.max(int1, int2);
+        
+        for (i = start; i <= end; i++ ){
+            sum += i;
+        }
+        return sum;
+    }
 };
 
 // Do not edit below this line
