67d49e5f1c76665319a250b0af56c34bd33314ce
Paul <paulschwandt3@gmail.com>
2023-01-23 19:16:00 -0700

    complete reverse string

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...values) {
+    let filteredArray = array.filter(item => !values.includes(item))
+    return filteredArray;
 };
 
 // Do not edit below this line
